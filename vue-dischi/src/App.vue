d3fa2126cf7037de4f6b50d70cd5613201d5c212
AntonioFBove <antoniofbove@gmail.com>
2022-01-31 15:32:29 +0100

    setup + components initialisation

---
+++ vue-dischi/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <the-main />
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+import TheMain from './components/TheMain.vue'
+
+export default {
+  name: "App",
+  components: {
+    TheMain
+  },
+  data() {
+    return {
+      discoteca: [],
+    }
+  },
+  mounted() {
+    axios
+    .get('https://flynn.boolean.careers/exercises/api/array/music')
+    .then((result) => {
+      this.discoteca = result.data.response;
+    });
+  }
+};
+</script>
+
+<style lang="scss">
+@import './style/main.scss'
+</style>
